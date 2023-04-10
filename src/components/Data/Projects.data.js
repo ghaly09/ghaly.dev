580f1ee69eef3cc1d91eaf49283780286d1261db
ghaly09 <rachmatghaly0997@gmail.com>
2023-04-10 21:11:45 +0700

    Data: Add some data object for projects

---
+++ src/components/Data/Projects.data.js
@@ -0,0 +1,68 @@
+export const projectsAttribute = [
+  {
+    id: 1,
+    title: "UI/UX FindThing Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+    description:
+      "Findthing is a UI/UX project design with an application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff, he/she can give it back to you by detecting the device information using Bluetooth system, QR Code, or Number ID.",
+    propjectTeam: "MasihMikir Team",
+    date: "July 13, 2021",
+    readMoreLink: "",
+  },
+  {
+    id: 2,
+    title: "UI/UX Datamoon Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230816587-d3bef426-a84e-4dde-a3ad-de50278a12ab.png",
+    description:
+      "Datamoon is a platform for finding the Datasets and processing them to become the benefit of businesses, companies, and science. In this project, I designed all of the necessary pages and research all the things with my team to fill the content of the website.",
+    propjectTeam: "Moonlight Team",
+    date: "January 20, 2022",
+    readMoreLink: "",
+  },
+  {
+    id: 3,
+    title: "Datamoon Logo Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230816891-163520be-8902-43f2-a014-d2f70df8ddf3.jpg",
+    description:
+      "My pleasure could join the Datamoon team for creating this logo design. I think putting all of the data stuff in this logo is based on the philosophy.",
+    propjectTeam: "Moonlight Team",
+    date: "Desember 25, 2021",
+    readMoreLink: "",
+  },
+  {
+    id: 4,
+    title: "Halotech.in Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230816950-197eadb0-c6ef-4ad5-909f-d8975261cdf8.png",
+    description:
+      "HaloTechin is a youtube channel focused on sharing knowledge, tips & tricks, and a community about Technology for everyone who wants to learn about Updated Tech. Subscribe to our channel and be powerful!.",
+    propjectTeam: "Moonlight Team",
+    date: "May 10, 2022",
+    readMoreLink: "",
+  },
+  {
+    id: 5,
+    title: "KKN Tegalwaru Logo Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+    description:
+      "I designed a unique logo for Kuliah Kerja Nyata (KKN) team program in Tegal Waru Village from IPB University.",
+    propjectTeam: "KKN Tegalwaru Team",
+    date: "May 25, 2020",
+    readMoreLink: "",
+  },
+  {
+    id: 6,
+    title: "KKN Tegalwaru Video Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+    description:
+      "I made The Profile Video for UMKM with my Kuliah Kerja Nyata (KKN) team program in Tegal Waru Village from IPB University.",
+    propjectTeam: "KKN Tegalwaru Team",
+    date: "June 18, 2020",
+    readMoreLink: "",
+  },
+];
