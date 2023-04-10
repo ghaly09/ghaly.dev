fd58e69649dba342b1056b29d0e15d80a855711d
ghaly09 <rachmatghaly0997@gmail.com>
2023-04-10 21:11:08 +0700

    Code: Making Projects Page and Card Project

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,3 +1,35 @@
+import { Card } from "../templates/Card";
+import { projectsAttribute } from "../Data/Projects.data";
+import { Footer } from "../templates/Footer";
+
 export const Projects = () => {
-  return <div></div>;
+  return (
+    <>
+      <section id="projects">
+        <div className="px-5 lg:px-0 pt-20 md:pt-32">
+          <h2 className="font-ghaly text-3xl lg:text-5xl font-bold pb-3 text-transparent bg-clip-text bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]">
+            Projects
+          </h2>
+          <h2 className="font-ghaly text-base lg:text-md pb-3 text-[#d3d4d6] ">
+            Showcase of my works on frontend development and Design.
+          </h2>
+        </div>
+        <div className="flex flex-col-reverse md:flex-row flex-wrap justify-between items-start px-5 lg:px-0 ">
+          {projectsAttribute.map((project) => {
+            return (
+              <Card
+                title={project.title}
+                imgSrc={project.imgSrc}
+                description={project.description}
+                propjectTeam={project.propjectTeam}
+                date={project.date}
+                readMoreLink={project.readMoreLink}
+              />
+            );
+          })}
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 };
---
+++ src/components/templates/Card.jsx
@@ -0,0 +1,65 @@
+export const Card = (props) => {
+  return (
+    <div
+      id="box"
+      className="line-effect font-ghaly max-w-[21rem] scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow"
+    >
+      <div className="border-[1px] border-slate-600 bg-[#0e1111] md:shadow-xl rounded-md my-1.5">
+        <a target="_blank" rel="noreferrer noopener" href={props.readMoreLink}>
+          <div className="md:flex-shrink-0 ">
+            <img
+              src={props.imgSrc}
+              alt={props.title}
+              className="object-cover h-[200px] w-full rounded-lg rounded-b-none"
+            />
+          </div>
+        </a>
+
+        <div className="py-1">
+          <div className="p-4">
+            <h2 className="truncate font-bold mb-2 md:mt-1 text-xl text-slate-200 tracking-normal">
+              {props.title}
+            </h2>
+
+            <p className="truncate hover:text-clip hover:overflow-hidden break-words text-sm text-[#d3d4d6] px-1">
+              {props.description}
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between py-2 md:py-4 md:mx-4">
+            {/* <a
+              target="_blank"
+              rel="noreferrer noopener"
+              href={props.readMoreLink}
+            >
+              <div className="flex items-center">
+                <div className="text-sm ml-2">
+                  <p className="text-[#d3d4d6] leading-none">
+                    {props.propjectTeam}
+                  </p>
+
+                  <p className="text-slate-400">{props.date}</p>
+                </div>
+              </div>
+            </a> */}
+            <a
+              id="readMore"
+              target="_blank"
+              rel="noreferrer noopener"
+              href={props.readMoreLink}
+              className="text-white text-sm hover-underline-animation"
+            >
+              Read More{" "}
+              <span>
+                <i
+                  className="fa fa-long-arrow-right text-[11px]"
+                  aria-hidden="true"
+                ></i>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
