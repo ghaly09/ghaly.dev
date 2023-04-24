ca0f040770c9aeaa4446be034451c51cad513fa2
ghaly09 <rachmatghaly0997@gmail.com>
2023-04-24 23:56:31 +0700

    Data: Making Project's Data

--- src/components/Data/Projects.data.js
+++ src/components/Data/Projects.data.js
@@ -1,28 +1,6 @@
 export const projectsAttribute = [
   {
     id: 1,
-    title: "UI/UX FindThing Project",
-    imgSrc:
-      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
-    description:
-      "Findthing is a UI/UX project design with an application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff, he/she can give it back to you by detecting the device information using Bluetooth system, QR Code, or Number ID.",
-    propjectTeam: "MasihMikir Team",
-    date: "July 13, 2021",
-    readMoreLink: "",
-  },
-  {
-    id: 2,
-    title: "UI/UX Datamoon Project",
-    imgSrc:
-      "https://user-images.githubusercontent.com/60167960/230816587-d3bef426-a84e-4dde-a3ad-de50278a12ab.png",
-    description:
-      "Datamoon is a platform for finding the Datasets and processing them to become the benefit of businesses, companies, and science. In this project, I designed all of the necessary pages and research all the things with my team to fill the content of the website.",
-    propjectTeam: "Moonlight Team",
-    date: "January 20, 2022",
-    readMoreLink: "",
-  },
-  {
-    id: 3,
     title: "Datamoon Logo Project",
     imgSrc:
       "https://user-images.githubusercontent.com/60167960/230816891-163520be-8902-43f2-a014-d2f70df8ddf3.jpg",
@@ -33,7 +11,7 @@ export const projectsAttribute = [
     readMoreLink: "",
   },
   {
-    id: 4,
+    id: 2,
     title: "Halotech.in Project",
     imgSrc:
       "https://user-images.githubusercontent.com/60167960/230816950-197eadb0-c6ef-4ad5-909f-d8975261cdf8.png",
@@ -44,10 +22,10 @@ export const projectsAttribute = [
     readMoreLink: "",
   },
   {
-    id: 5,
+    id: 3,
     title: "KKN Tegalwaru Logo Project",
     imgSrc:
-      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      "https://user-images.githubusercontent.com/60167960/231047262-42137256-16aa-4b50-8e54-aae5b16ede08.png",
     description:
       "I designed a unique logo for Kuliah Kerja Nyata (KKN) team program in Tegal Waru Village from IPB University.",
     propjectTeam: "KKN Tegalwaru Team",
@@ -55,14 +33,47 @@ export const projectsAttribute = [
     readMoreLink: "",
   },
   {
-    id: 6,
+    id: 4,
     title: "KKN Tegalwaru Video Project",
     imgSrc:
-      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      "https://user-images.githubusercontent.com/60167960/231047312-26cdb3fa-89e4-4d17-91fc-22f2e7cb74e8.jpg",
     description:
       "I made The Profile Video for UMKM with my Kuliah Kerja Nyata (KKN) team program in Tegal Waru Village from IPB University.",
     propjectTeam: "KKN Tegalwaru Team",
     date: "June 18, 2020",
+    readMoreLink: "",
+  },
+  {
+    id: 5,
+    title: "UI/UX Datamoon Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230816587-d3bef426-a84e-4dde-a3ad-de50278a12ab.png",
+    description:
+      "Datamoon is a platform for finding the Datasets and processing them to become the benefit of businesses, companies, and science. In this project, I designed all of the necessary pages and research all the things with my team to fill the content of the website.",
+    propjectTeam: "Moonlight Team",
+    date: "January 20, 2022",
+    readMoreLink: "",
+  },
+  {
+    id: 6,
+    title: "UI/UX FindThing Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+    description:
+      "Findthing is a UI/UX project design with an application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff, he/she can give it back to you by detecting the device information using Bluetooth system, QR Code, or Number ID.",
+    propjectTeam: "MasihMikir Team",
+    date: "July 13, 2021",
+    readMoreLink: "",
+  },
+  {
+    id: 7,
+    title: "Calculator Javascript Project",
+    imgSrc:
+      "https://user-images.githubusercontent.com/60167960/232182820-e7f74f49-6a7f-497f-9472-0bcce56d8610.png",
+    description:
+      "After I learned about DOM (Document Object Model), I try to make a mini project to apply my knowledge, and Woala! here it this!",
+    propjectTeam: "Self Project",
+    date: "July 10, 2022",
     readMoreLink: "",
   },
 ];
